4c35fae45b0639dfa9d66953a788c8a7ee84fadb
Dan Abramov <dan.abramov@gmail.com>
2013-05-16 20:37:10 +0400

    Add several helpers

--- UIKit/UIKitExtensions.cs
+++ UIKit/UIKitExtensions.cs
@@ -1,28 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using MonoTouch;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
 
 namespace Stampsy.Extensions.UIKit
 {
     public static class UIKitExtensions
     {
+        [DllImport (Constants.ObjectiveCLibrary)]
+        static extern IntPtr object_getClassName (IntPtr obj);
+
+        [DllImport (Constants.SystemLibrary)]
+        static extern int sysctlbyname ([MarshalAs(UnmanagedType.LPStr)] string property, IntPtr output, IntPtr oldLen, IntPtr newp, uint newlen);
+
+        const string HardwareProperty = "hw.machine";
+
         /// <summary>
-        /// Makes network indicator visible until <paramref name="task"/> is completed.
-        /// If <paramref name="task"/> is already completed, nothing happens.
+        /// Returns Objective C class name for a given object.
         /// </summary>
-        public static Task WithNetworkIndicator (this Task task)
-        {
-            NetworkIndicator.RegisterTask (task);
-            return task;
+        public static string GetClassName (this NSObject o) {
+            return Marshal.PtrToStringAuto (object_getClassName (o.Handle));
         }
 
         /// <summary>
-        /// Makes network indicator visible until <paramref name="task"/> is completed.
-        /// If <paramref name="task"/> is already completed, nothing happens.
+        /// Returns a platform identifier, such as "iPhone5,2", "iPad2,5" or "x86_64".
         /// </summary>
-        public static Task<T> WithNetworkIndicator<T> (this Task<T> task)
+        /// <remarks>
+        /// See an up-to-date list of identifiers and platforms they correspond to:
+        /// http://stackoverflow.com/a/3950748/458193
+        /// </remarks>
+        public static string GetPlatformIdentifier (this UIDevice device)
         {
-            NetworkIndicator.RegisterTask (task);
-            return task;
+            var pLen = Marshal.AllocHGlobal (sizeof (int));
+            sysctlbyname (HardwareProperty, IntPtr.Zero, pLen, IntPtr.Zero, 0);
+
+            var length = Marshal.ReadInt32 (pLen);
+
+            var pStr = Marshal.AllocHGlobal (length);
+            sysctlbyname (HardwareProperty, pStr, pLen, IntPtr.Zero, 0);
+
+            return Marshal.PtrToStringAnsi (pStr);
         }
 
         /// <summary>
@@ -56,6 +77,50 @@ namespace Stampsy.Extensions.UIKit
             var bg = new BackgroundTaskHandle ();
             task.ContinueWith (t => bg.Dispose (), TaskContinuationOptions.ExecuteSynchronously);
 
+            return task;
+        }
+
+        /// <summary>
+        /// Creates an index path from <paramref name="index"/>.
+        /// </summary>
+        public static NSIndexPath ToRow (this int index)
+        {
+            return NSIndexPath.FromRowSection (index, 0);
+        }
+
+        /// <summary>
+        /// Creates a single-element index path array from <paramref name="index"/>.
+        /// </summary>
+        public static NSIndexPath [] ToRows (this int index)
+        {
+            return new [] { index.ToRow () };
+        }
+
+        /// <summary>
+        /// Creates an index path array from a sequence of indices.
+        /// </summary>
+        public static NSIndexPath [] ToRows (this IEnumerable<int> indices)
+        {
+            return indices.Select (ToRow).ToArray ();
+        }
+
+        /// <summary>
+        /// Makes network indicator visible until <paramref name="task"/> is completed.
+        /// If <paramref name="task"/> is already completed, nothing happens.
+        /// </summary>
+        public static Task WithNetworkIndicator (this Task task)
+        {
+            NetworkIndicator.RegisterTask (task);
+            return task;
+        }
+
+        /// <summary>
+        /// Makes network indicator visible until <paramref name="task"/> is completed.
+        /// If <paramref name="task"/> is already completed, nothing happens.
+        /// </summary>
+        public static Task<T> WithNetworkIndicator<T> (this Task<T> task)
+        {
+            NetworkIndicator.RegisterTask (task);
             return task;
         }
     }
